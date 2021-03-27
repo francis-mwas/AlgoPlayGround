e0ba97ebf046772a3a2c3078408f6d45db6e122d
francis-mwas <mwangif184@gmail.com>
2021-03-27 15:18:58 +0300

    factorial using recursive solution

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -1,13 +1,25 @@
 // Non-recursive solution 
 function fact(num) {
-    let total = 1;
+
+    if (num < 0) return false;
+    let product = 1;
     if (num === undefined) return false;
     for (let i = num; i > 0; i--) {
-        total *= i;
+        product *= i;
     }
-    return total;
+    return product;
 }
 
 // recursive solution 
+// factorial(4) returns 4 * factorial(3)
+// factorial(3) returns 4 * 3 * factorial(2)
+// factorial(2) returns 4 * 3 * 2 * factorial(1)
+// factorial(1) returns 4 * 3 * 2 * 1 * factorial(0)
+// factorial(0) returns 4 * 3 * 2 * 1 * 1
+function factorial(num) {
+    if (num < 0) return false;
+    if (num === 1) return 1;
+    return num * factorial(num - 1);
+}
 
-console.log(fact(4));
+console.log(factorial(4));
